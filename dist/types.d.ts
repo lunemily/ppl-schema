b759014cb7912f16d0fe559baeb620b1fe7fe1ac
Emily Blanchette <emilylunella@gmail.com>
2025-02-05 12:19:51 -0600

    1.0.3

--- dist/types.d.ts
+++ dist/types.d.ts
@@ -1,46 +1,34 @@
-export type Permission = object;
-export type User = {
+export type Account = {
     username?: string;
     password?: string;
     id: string;
-    permissions?: Permission[];
-    challengerId?: string;
-    leaderId?: string;
+    permissions?: Map<string, boolean>;
+    _links?: Map<string, string>;
 };
-export type BingoSpace = {
-    id: string;
-    earned: boolean;
-};
-export type BingoBoard = BingoSpace[][];
 export type Challenger = {
-    displayName: string;
-    bingoBoard: BingoBoard;
-} & User;
-export type BattleDifficulty = {
-    id: number;
-    name: string;
-};
-export type BattleFormat = {
-    id: number;
-    name: string;
+    id: string;
+    event?: string;
+    accountId?: string;
+    displayName?: string;
+    bingoBoard?: BingoBoard;
 };
 export type Leader = {
-    leaderName: string;
-    badgeName: string;
+    id: string;
+    event?: string;
+    accountIds?: string[];
+    leaderName?: string;
+    badgeName?: string;
     bio?: string;
     tagline?: string;
-    battleDifficulties?: number;
-    battleDifficultiesList?: BattleDifficulty[];
-    battleFormats?: number;
-    battleFormatsList?: BattleFormat[];
+    battleDifficulties?: BattleDifficulty[];
+    battleFormats?: BattleFormat[];
     battleCode?: string;
     queueOpen?: boolean;
     twitchHandle?: string;
-    queueOpenText?: string;
-    queueCloseText?: string;
-} & User;
+};
 export type Battle = {
     id: number;
+    event?: string;
     challengerId: string;
     leaderId: string;
     battleDifficulty: number;
@@ -51,8 +39,27 @@ export type Battle = {
     battleCode: string;
 };
 export type Report = {
+    id: number;
+    event?: string;
     challengerId: string;
     leaderId: string;
     notes: string;
     reportedUTC: number;
+};
+export type Event = {
+    name: string;
+    year: number;
+};
+export type BingoSpace = {
+    id: string;
+    earned: boolean;
+};
+export type BingoBoard = BingoSpace[][];
+export type BattleDifficulty = {
+    id: number;
+    name: string;
+};
+export type BattleFormat = {
+    id: number;
+    name: string;
 };
